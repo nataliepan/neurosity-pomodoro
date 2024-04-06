0e77d9446bec15343559aa1fc1b6ae0156aa3bd6
Natalie Pan <natalie.pan@gmail.com>
2024-04-06 16:28:01 -0700

    cors flask

--- server/main.py
+++ server/main.py
@@ -1,6 +1,10 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+cors = CORS(app, origins="*")
+# cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 @app.route('/api/v1/hello-world')
 def hellp_world():
