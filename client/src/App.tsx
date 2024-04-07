5f859d73a42215a0215bdf633cc1bdda3baf0ab0
Natalie Pan <natalie.pan@gmail.com>
2024-04-06 17:07:12 -0700

    hello world working in client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const fetchHello = async () => {
+    const response = await axios.get("http://localhost:8080/api/v1/hello-world");
+    console.log(response.data.message);
+  };
+
+  useEffect(() => {
+    fetchHello();
+  }, []);
 
   return (
     <>
@@ -18,18 +28,14 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
